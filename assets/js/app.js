5221783aa6ea3a67539d96fcacfaae43ae5dd00e
Alexis D. Zazueta <azazueta@MacBook-Pro-de-Alexis.local>
2018-05-27 20:05:56 -0600

    Manifest

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ $(function() {
    var navbar = $(".navbar");
    $(window).scroll(function() {    
        var scroll = $(window).scrollTop();
-       if (scroll >= 50) {
+       if (scroll >= 100) {
            navbar.addClass("scrolled");
        } else {
            navbar.removeClass("scrolled");
